753e47bc4648bf8a128eaee3f6cf57c0c9cc6492
Herman Khivrenko <germankhivrenko@gmail.com>
2022-05-25 20:33:55 +0300

    added mongo driver

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require('dotenv').config()
+const {MongoClient} = require('mongodb')
 const {createScraper} = require('./wog')
 
-const scraper = createScraper()
 ;(async () => {
-  const stations = await scraper.scrapeStations()
-
-  for await (const station of stations) {
-    console.dir(station)
+  try {
+    const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}`
+      + `@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+    const client = new MongoClient(mongoURL)
+    await client.connect()
+    const db = client.db(process.env.MONGO_DB)
+  } catch(err) {
+    console.error(err)
+    throw err
   }
 })()
 
