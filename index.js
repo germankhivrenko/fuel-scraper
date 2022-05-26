48b16a6737b49ce04a89238b723c23ba3db01d78
Herman Khivrenko <germankhivrenko@gmail.com>
2022-05-26 20:57:39 +0300

    added one scraper iteration run

--- index.js
+++ index.js
@@ -4,35 +4,43 @@ const {Job} = require('./job')
 const {createScraper} = require('./wog')
 
 ;(async () => {
-  const wogScraperJob = Job.create(async () => {
-    console.time()
-    const wogScraper = createScraper()
-    const stationsData = await wogScraper.scrapeStations()
+  try {
+    // setup mongo client
+    const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}`
+      + `@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+    const client = new MongoClient(mongoURL)
+    console.log('hello')
+    await client.connect()
+    console.log('Successfully connected')
+    const db = client.db(process.env.MONGO_DB)
+    const coll = db.collection('station')
+    
+    // setup scraper job
+    const job = Job.create(async () => {
+      const wogScraper = createScraper()
+      const stationsData = await wogScraper.scrapeStations()
 
-    for await (const station of stationsData) {
-      console.dir(station, {depth: 3})
+      for await (const station of stationsData) {
+        const filter = {brand: station.brand, externalId: station.externalId}
+        const update = {$set: station}
+        const options = {upsert: true}
 
-      // TODO: create or update in db
-    }
+        await coll.updateOne(filter, update, options)
+      }
+    }, 5 * 60 * 1000)
+    
+    // run one scraper iteration
+    job.start()
+    console.log('Start stopping')
+    await job.stop()
+    console.log('Successfully stopped')
 
-    console.log('======= FINISHED ONE ITERATION =======')
-    console.timeEnd()
-  }, 5 * 60 * 1000)
-  wogScraperJob.start()
-  console.log('Start stopping')
-  await wogScraperJob.stop()
-  console.log('Successfully stopped')
-
-  // try {
-  //   const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}`
-  //     + `@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
-  //   const client = new MongoClient(mongoURL)
-  //   await client.connect()
-  //   const db = client.db(process.env.MONGO_DB)
-  // } catch(err) {
-  //   console.error(err)
-  //   throw err
-  // }
+    // close mongo connection
+    await client.close()
+    console.log('Successfully closed')
+  } catch(err) {
+    console.error(err)
+    throw err
+  }
 })()
-
 
